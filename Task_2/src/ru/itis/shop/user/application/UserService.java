d61d1b7a58d761a75238847ff967281e5cce870f
Bogdan <mitkobogdan@gmail.com>
2026-07-02 02:53:21 +0300

    Начало выполнения второго ДЗ

---
+++ Task_2/src/ru/itis/shop/user/application/UserService.java
@@ -0,0 +1,28 @@
+package ru.itis.shop.user.application;
+
+import ru.itis.shop.user.domain.User;
+import ru.itis.shop.user.repository.UserRepository;
+
+import java.util.Optional;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void signUp(String email, String password, String profileDescription) {
+        User user = new User(email, password, profileDescription);
+        userRepository.save(user);
+    }
+
+    public boolean signIn(String email, String password) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        if (userOptional.isPresent()) {
+            return userOptional.get().getPassword().equals(password);
+        } else return false;
+    }
+}
---
+++ Task_2/src/ru/itis/shop/user/infrastructure/persistence/UserFileRepository.java
@@ -0,0 +1,61 @@
+package ru.itis.shop.user.infrastructure.persistence;
+
+import ru.itis.shop.user.domain.User;
+import ru.itis.shop.user.repository.UserRepository;
+
+import java.io.*;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserFileRepository implements UserRepository {
+
+    private final String fileName;
+
+    private final UserMapper userMapper;
+
+    public UserFileRepository(String fileName, UserMapper userMapper) {
+        this.fileName = fileName;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public void save(User user) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            String id = UUID.randomUUID().toString();
+            user.setId(id);
+            writer.write(userMapper.toLine(user));
+            writer.newLine();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+
+            String line = reader.readLine();
+
+            while (line != null) {
+
+                User user = userMapper.fromLine(line);
+
+                if (user.getEmail().equals(email)) {
+                    return Optional.of(user);
+                }
+
+                line = reader.readLine();
+            }
+
+            return Optional.empty();
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return Optional.empty();
+    }
+}
